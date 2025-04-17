58861646d1019416a936429d04914c73e2feeebf
Roman-1994 <piskunov-1994@list.ru>
2025-04-17 10:34:00 +0300

    add new file

--- app.py
+++ app.py
@@ -1,10 +1,183 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, FileResponse
+import base64
+import uuid
+from typing import Annotated
+
+from fastapi import Cookie, Depends, FastAPI, Form, Header, HTTPException, Request, Response, status
+from fastapi.responses import FileResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from itsdangerous import Signer
+
+import config
+from logger import logger
+from models.models import Feedback, Password, User, Worker
+
+
+print(base64.b64encode(bytes("user1:pass1", "utf-8")))
 
 app = FastAPI()
+security = HTTPBasic()
 
 
 @app.get("/")
 async def root():
-    #return {"message": "Авторелоад действительно работает"}
-    return FileResponse('start.html')
+    logger.info("Handling request to root endpoint")
+    return FileResponse("start.html")
+
+
+@app.post("/calculate")
+async def calculate(num1, num2):
+    return {"result": int(num1) + int(num2)}
+
+
+@app.get("/db")
+async def get_db_info():
+    logger.info(f"Connecting to database: {config.load_config().db.database_url}")
+    return {"database_url": config.load_config().db.database_url}
+
+
+@app.get("/users")
+async def get_user():
+    user = User(id=1, name="John Doe")
+    logger.info("get users")
+    return {"user_name": user.name, "user_id": user.id}
+
+
+@app.post("/user")
+async def post_user(user: User):
+    return user
+
+
+fake_db = [{"username": "vasya", "user_info": "любит колбасу"}, {"username": "katya", "user_info": "любит петь"}]
+
+
+# Обрабатываем GET-запрос, чтобы вернуть список пользователей
+@app.get("/get_users")
+async def get_all_users():
+    return fake_db
+
+
+@app.get("/get_user/{user_id}")
+async def get_user_id(user_id: int):
+    return {"username": fake_db[user_id]["username"], "user_info": fake_db[user_id]["user_info"]}
+
+
+# Обрабатываем POST-запрос, чтобы добавить нового пользователя
+@app.post("/add_user", response_model=Worker)
+async def add_user(user: Worker):
+    fake_db.append({"username": user.username, "user_info": user.user_info})
+    print(fake_db)
+    return user
+
+
+fake_feedback = []
+
+
+@app.post("/feedback")
+async def add_feedback(feedback: Feedback, is_premium=False):
+    if is_premium:
+        return {
+            "response": feedback.check_message(),
+            "name": feedback.name,
+            "message": feedback.message,
+            "email": feedback.contact.email,
+            "phone": feedback.contact.phone,
+            "is_premium": "Ваш отзыв будет рассмотрен в приоритетном порядке",
+        }
+    else:
+        return {
+            "response": feedback.check_message(),
+            "name": feedback.name,
+            "message": feedback.message,
+            "email": feedback.contact.email,
+            "phone": feedback.contact.phone,
+        }
+
+
+@app.post("/submit/")
+async def submit_form(username: str = Form(...), password: str = Form(...)):
+    return {"username": username, "password_length": len(password)}
+
+
+sample_product_1 = {"product_id": 123, "name": "Smartphone", "category": "Electronics", "price": 599.99}
+
+sample_product_2 = {"product_id": 456, "name": "Phone Case", "category": "Accessories", "price": 19.99}
+
+sample_product_3 = {"product_id": 789, "name": "Iphone", "category": "Electronics", "price": 1299.99}
+
+sample_product_4 = {"product_id": 101, "name": "Headphones", "category": "Accessories", "price": 99.99}
+
+sample_product_5 = {"product_id": 202, "name": "Smartwatch", "category": "Electronics", "price": 299.99}
+
+sample_products = [sample_product_1, sample_product_2, sample_product_3, sample_product_4, sample_product_5]
+
+session_token = []
+
+secret = config.load_login()
+secret_user = Signer(secret_key=secret, sep=".")
+
+
+@app.post("/login")
+async def user_login(password: Password, response: Response, user_agent: Annotated[str | None, Header()] = None):
+    if password.username == "admin" and password.password == "123qwe":
+        user_uuid = uuid.uuid4()
+        secret_user.sign(str(user_uuid))
+        response.set_cookie(key="session_token", value=secret_user.sign(str(user_uuid)), httponly=True, max_age=360)
+        print(user_uuid)
+        print(secret_user.sign(str(user_uuid)))
+        print(secret_user.unsign(secret_user.sign(str(user_uuid))))
+        return {"message": "Get user_id", "user_agent": user_agent}
+    else:
+        return {"message": "Error login"}
+
+
+@app.get("/product/{product_id}")
+async def get_product(product_id: int, session_token=Cookie()):
+    print(secret_user.get_signature(session_token))
+    if secret_user.get_signature(session_token):
+        for product in sample_products:
+            if product_id == product["product_id"]:
+                return product
+    else:
+        return {"ERROR": "ERROR"}
+
+
+@app.get("/products/search/")
+async def product_search(keyword: str, category: str = "", limit: int = 10):
+    product_list = []
+    for product in sample_products:
+        if keyword.lower() in product["name"].lower() and category.lower() == product["category"].lower():
+            product_list.append(product)
+    return product_list[:limit]
+
+
+@app.get("/headers")
+async def get_headers(headers: Request):
+    if headers.headers["accept-language"]:
+        return {"user_agent": headers.headers["user-agent"], "accept_language": headers.headers["accept-language"]}
+    else:
+        return HTTPException(status_code=400, detail="Not headers accept_language")
+
+
+USER_DATA = [
+    Password(**{"username": "user1", "password": "pass1"}),
+    Password(**{"username": "user2", "password": "pass2"}),
+]
+
+
+def get_user_from_db(username: str):
+    for user in USER_DATA:
+        if user.username == username:
+            return user
+    return None
+
+
+def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
+    user = get_user_from_db(credentials.username)
+    if user is None or user.password != credentials.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    return user
+
+
+@app.get("/protected_resource/")
+def get_protected_resource(user: Password = Depends(authenticate_user)):
+    return {"message": "You have access to the protected resource!", "user_info": user}
---
+++ models/models.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, EmailStr, Field, computed_field, field_validator
+from pydantic_extra_types.phone_numbers import PhoneNumber
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+    @computed_field
+    @property
+    def check_age(self) -> bool:
+        return self.age >= 18
+
+
+class Worker(BaseModel):
+    username: str
+    user_info: str
+
+
+class Contact(BaseModel):
+    email: EmailStr
+    phone: PhoneNumber
+
+
+class Feedback(BaseModel):
+    name: str = Field(min_length=2, max_length=50)
+    message: str = Field(min_length=10, max_length=500)
+    contact: Contact
+
+    @field_validator("message")
+    def check_message(msg):
+        if "редис" in msg:
+            raise ValueError("Сообщение содержит запретное слово")
+        return "Ваш отзыв сохранён."
+
+
+class Password(BaseModel):
+    username: str
+    password: str
